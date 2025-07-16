f386c7e27892710b4ee3d04018e170018e6ce5fe
SeidoTaki <seidotakizava30@gmail.com>
2025-07-17 00:23:21 +0300

    Delete Practice directory

--- Practice/client/src/components/SummarySection.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ActivitySummary from './ActivitySummary';
-import ActivityRating from './ActivityRating';
-export default function SummarySection({ userId }) {
-  return (
-      userId
-  );
-}
